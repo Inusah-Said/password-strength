e0d49759da547accd61d65837f5dc32735cbe69c
Inusah Said <saidinusah29@gmail.com>
2021-10-11 08:07:07 +0000

    basic changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,71 @@
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import './index.css';
+import checkStrength from './utils/passwordStrength';
+import { LinearProgress, TextField } from '@material-ui/core';
 
 function App() {
+	const [password, setpassword] = useState('');
+	const [strength, setStrength] = useState('');
+	const [progress, setProgess] = useState(0);
+
+	const handleChange = (e) => {
+		const { name, value } = e.target;
+		const result = checkStrength(password);
+
+		if (name === 'newPassword') {
+			if (value === '') {
+				setProgess(0);
+				setStrength('');
+			} else {
+				setStrength(result.strength);
+				setProgess(result.progress);
+			}
+		}
+
+		setpassword(value);
+	};
+
 	return (
-		<div className="App">
-			<h2>Password strength</h2>
+		<div>
+			<h2 className="text-orange">Password strength</h2>
+
+			<div className="w-1/5">
+				<form className="space-y-2">
+					<div>
+						<TextField
+							id="newPassword"
+							name="newPassword"
+							type="password"
+							placeholder="New Password"
+							label="New Password"
+							onChange={handleChange}
+							value={password}
+							disableUnderline
+							fullWidth
+						/>
+
+						<LinearProgress
+							variant="determinate"
+							value={progress}
+							className={{
+								root: 'bg-primary mt-px w-1/2',
+								bar: `${
+									strength === 'Weak'
+										? 'bg-appRed'
+										: strength === 'Good'
+										? 'bg-appBlue'
+										: strength === 'Strong'
+										? 'bg-green'
+										: 'bg-thinGrayBG'
+								} `,
+							}}
+						/>
+						<p>Password Strength is {strength}</p>
+					</div>
+				</form>
+			</div>
 		</div>
 	);
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,36 @@
 module.exports = {
+	mode: 'jit',
 	purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
 	darkMode: false, // or 'media' or 'class'
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				primary: '#171725',
+				secondary: '#06B2BC',
+				accent: '#FF3F24',
+				appGreen: '#38C985',
+				appBlue: '#477DFB',
+				appRed: '#FF3D22',
+				appGray: '#9CA7B8',
+				appBlack: '#040A1D',
+				orange: '#F3641F',
+				blue: '#477DFB',
+				red: '#FF3D22',
+				green: '#38C985',
+				gray1: '#040A1D',
+				gray2: '#4C536A',
+				gray3: '#9CA7B8',
+				grayBg: '#E5E5E5',
+				thinGrayBG: '#EEF2F5',
+				cardHeading: '#3D4356',
+			},
+			screens: {
+				customH: { raw: '(max-height: 880px)' },
+			},
+		},
 	},
 	variants: {
 		extend: {},
+		outline: ['focus'],
 	},
-	plugins: [],
 };
